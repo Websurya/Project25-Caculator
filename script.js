20ac8db7d0edc6fdf324e36c394a8281d2eb2c39
Suriyakumar <surya.conversion@gmail.com>
2024-10-02 19:00:04 +0530

    Simple Calculator

---
+++ script.js
@@ -0,0 +1,12 @@
+function add(){
+    document.getElementById("addoutput").innerHTML = parseInt(document.getElementById("input1").value) + parseInt(document.getElementById("input2").value);
+}
+function sub(){
+    document.getElementById("addoutput").innerHTML = parseInt(document.getElementById("input1").value) - parseInt(document.getElementById("input2").value);
+}
+function mul(){
+    document.getElementById("addoutput").innerHTML = parseInt(document.getElementById("input1").value) * parseInt(document.getElementById("input2").value);
+}
+function div(){
+    document.getElementById("addoutput").innerHTML = parseInt(document.getElementById("input1").value) / parseInt(document.getElementById("input2").value);
+}
